338834441e850a3b07b7223b06d02c877479a27d
Jonathan Rockway <jon@jrock.us>
2011-04-18 01:45:44 -0500

    much refactoring for easier subclassing

--- lib/GraphViz/HasA/Introspect.pm
+++ lib/GraphViz/HasA/Introspect.pm
@@ -56,92 +56,102 @@ sub extract_classnames_from_docstring {
     return map { $self->extract_classnames_from_type($_) } @types;
 }
 
-sub find_links_from_class {
-    my ($self, $class) = @_;
+sub find_links_from_attribute {
+    my ($self, $class, $attr) = @_;
+    my $name = $attr->name;
+    my $longname = join '::', $class->name, $name;
+
+    my (%result, @classnames);
+
+    $result{weak_ref} = 1 if $attr->is_weak_ref;
+
+    # optional means that this thing may never exist, and
+    # that's okay
+    $result{optional} = 1
+        unless $attr->is_required ||
+               $attr->has_default ||
+               $attr->has_builder;
+
+    try {
+        if($attr->has_documentation) {
+            push @classnames, $self->extract_classnames_from_docstring(
+                $attr->documentation,
+            );
+        }
+    }
+    catch {
+        warn "warning: cannot load constraints from docstring of $longname: $_";
+    };
+
+    try {
+        if($attr->has_type_constraint) {
+            push @classnames, $self->extract_classnames_from_type(
+                $attr->type_constraint,
+            );
+        }
+    }
+    catch {
+        warn "warning: cannot load type constraint for $longname: $_";
+    };
 
     my @links;
 
-    for my $attr (sort { $a->name cmp $b->name } $class->get_all_attributes){
-        my $name = $attr->name;
-        my $longname = join '::', $class->name, $name;
-
-        my (%result, @classnames);
-
-        $result{weak_ref} = 1 if $attr->is_weak_ref;
-
-        # optional means that this thing may never exist, and
-        # that's okay
-        $result{optional} = 1
-            unless $attr->is_required ||
-                   $attr->has_default ||
-                   $attr->has_builder;
-
-        try {
-            if($attr->has_documentation) {
-                push @classnames, $self->extract_classnames_from_docstring(
-                    $attr->documentation,
-                );
-            }
-        }
-        catch {
-            warn "warning: cannot load constraints from docstring of $longname: $_";
-        };
-
-        try {
-            if($attr->has_type_constraint) {
-                push @classnames, $self->extract_classnames_from_type(
-                    $attr->type_constraint,
-                );
-            }
-        }
-        catch {
-            warn "warning: cannot load type constraint for $longname: $_";
-        };
-
-        for my $cname (@classnames) {
-            push @links, [ $name => { %result, to => $cname } ];
-        }
+    for my $cname (@classnames) {
+        push @links, [ $name => { %result, to => $cname } ];
     }
 
     return @links;
+}
+
+sub find_attributes_in_class {
+    my ($self, $class) = @_;
+    return sort { $a->name cmp $b->name } $class->get_all_attributes;
+}
+
+sub find_links_from_class {
+    my ($self, $class) = @_;
+
+    return map { $self->find_links_from_attribute($class, $_) }
+        $self->find_attributes_in_class($class);
+}
+
+sub find_attributes_in_role {
+    my ($self, $role) = @_;
+    return sort { $a cmp $b }
+        uniq map { $_->get_attribute_list } $role->calculate_all_roles;
+}
+
+sub find_links_from_role_attribute {
+    my ($self, $role, $name) = @_;
+    my $longname = join '::', $role->name, $name;
+    my $attr = $role->get_attribute($name);
+
+    my $type = $attr->{isa};
+    return unless $type;
+
+    my (%result, @classnames);
+    $result{weak_ref} = 1 if $attr->{weak_ref};
+    $result{optional} = 1
+        unless $attr->{required}   ||
+               $attr->{default}    ||
+               $attr->{builder}    ||
+               $attr->{lazy_build};
+
+    try {
+        @classnames = $self->extract_classnames_from_type($type);
+    }
+    catch {
+        warn "cannot extract types from role attribute $longname: $_";
+    };
+
+    return map { [ $name => { %result, to => $_ } ] } @classnames;
 }
 
 sub find_links_from_role {
     my ($self, $role) = @_;
-
-    my @links;
-
-    my @attrs = uniq map { $_->get_attribute_list } $role->calculate_all_roles;
-  attr: for my $name (sort { $a cmp $b } @attrs) {
-        my $longname = join '::', $role->name, $name;
-        my $attr = $role->get_attribute($name);
-
-        my $type = $attr->{isa};
-        next attr unless $type;
-
-        my (%result, @classnames);
-        $result{weak_ref} = 1 if $attr->{weak_ref};
-        $result{optional} = 1
-            unless $attr->{required}   ||
-                   $attr->{default}    ||
-                   $attr->{builder}    ||
-                   $attr->{lazy_build};
-
-        try {
-            @classnames = $self->extract_classnames_from_type($type);
-        }
-        catch {
-            warn "cannot extract types from role attribute $longname: $_";
-        };
-
-        for my $cname (@classnames) {
-            push @links, [ $name => { %result, to => $cname } ];
-        }
-    }
-
-    return @links;
+    return map { $self->find_links_from_role_attribute($role, $_) }
+        $self->find_attributes_in_role($role);
 }
-
 
 sub find_links_from {
     my ($self, $it) = @_;
